c4dc303dfd49f1d35e25e34222c3b45827f85ed1
Justin <justin.jchun@gmail.com>
2016-11-09 14:03:11 -0800

    Brandon is a poo

--- app/src/main/java/com/example/armint/testtutor/MainActivity.java
+++ app/src/main/java/com/example/armint/testtutor/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //I'm adding this to piss armin off -justin
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
