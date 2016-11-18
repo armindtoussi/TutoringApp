62c9ebd26d6b264f3effb34aa0d1a89612a54396
Brandon Gillespie <bgillespie4@my.bcit.ca>
2016-11-18 14:44:52 -0800

    Random Commit

--- app/src/main/java/com/example/armint/testtutor/CreateAccountView.java
+++ app/src/main/java/com/example/armint/testtutor/CreateAccountView.java
@@ -5,19 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateAccountView extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_account_view);
     }
 
-    public void toAccount(View v) {
-        Intent i = new Intent(this, CreateAccountView.class);
-        startActivity(i);
+    public void cancel(View view) {
+        finish();
     }
 
-    public void onClick(View v) {
+    public void createAccount(View view) {
         Intent i = new Intent(this, UserView.class);
         startActivity(i);
     }
--- app/src/main/java/com/example/armint/testtutor/OptionsViewFrag.java
+++ app/src/main/java/com/example/armint/testtutor/OptionsViewFrag.java
@@ -1,5 +1,6 @@
 package com.example.armint.testtutor;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,9 +8,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class OptionsViewFrag extends Fragment {
+
+    // Variables for attempted ListView options ListView population.
+    /*
+    private ArrayAdapter<String> optionsAdapter;
+    private ArrayList<String> options;
+    private ListView lv; */
 
     //Required ctor.
     public OptionsViewFrag() {
@@ -20,6 +31,27 @@ public class OptionsViewFrag extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // At this current moment, this code does not break anything, but it also does it do its
+        // required function of populating the options ListView. I tried to understand and implement
+        // this based off of other ListView but didn't quite get it. I must now move onto other things,
+        // we can discuss on this week.
+        /*
+        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.fragment_options_view, null);
+
+        lv = (ListView)view.findViewById(R.id.optionList);
+        options = new ArrayList<String>();
+        options.add("Invite Friends");
+        options.add("Report a problem");
+        options.add("Edit Profile");
+        options.add("Change Password");
+        options.add("Logout");
+
+        optionsAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, options);
+
+        lv.setAdapter(optionsAdapter); */
+
     }
 
     @Override
