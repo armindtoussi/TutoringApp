29efd7698386ed53092287d5fc83e5924e047bf3
drwatley <atoussi@my.bcit.ca>
2016-11-18 19:55:06 -0800

    Added backend to account creation, classes and such. I also reformatted the create account page so the layout was nice on all phones, and added al resources to strings xml. and a bunch of other shit. anyway. sorry for using master.

--- app/src/main/java/com/example/armint/testtutor/Account.java
+++ app/src/main/java/com/example/armint/testtutor/Account.java
@@ -1,8 +1,49 @@
 package com.example.armint.testtutor;
 
+import android.widget.Toast;
+
 /**
  * Created by Armin T on 2016-11-18.
  */
 
 public class Account {
+
+    private String userName;
+    private String password;
+
+    public Account (String userName, String password) {
+        this.setUserName(userName);
+        this.setPassword(password);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    //probably dno't need this here.
+    public String getPassword() {
+        //this should be more cehck validity upon log in? not sure.
+        return null;
+    }
+
+
+    public void setUserName(String userName) {
+        final String MESSAGE = "User name cannot be null and must be great than 4 characters";
+        //we will need to change this later to make sure this user name doesn't exist.
+        if(userName != null && userName.length() > 4) {
+            this.userName = userName;
+        } else {
+
+            //Toast.makeText(CreateAccountView.class, MESSAGE, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void setPassword(String password) {
+        //will need to do ome regex checks or something later
+        if(password != null && password.length() > 6) {
+            this.password = password;
+        } else {
+            //throw new IncorrectUserNameException("password cannot be null or less than 6 characters");
+        }
+    }
 }
